50934f0c2a643fed8d6ef5fa9db7a2eb6e8674ba
kailashtuta <kailashtuta2000@gmail.com>
2022-03-13 21:04:21 +0530

    Added user meal Plan report to database

--- dietician/models.py
+++ dietician/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Report(models.Model):
+    name = models.CharField(max_length=200)
+    age = models.IntegerField()
+    weight = models.IntegerField()
+    height = models.IntegerField()
+    bmi = models.IntegerField(default=0)
+    gender = models.CharField(max_length=10)
+    exercise = models.CharField(max_length=10)
+    diabetic = models.CharField(max_length=10)
+    remark = models.CharField(max_length=50, default='', blank=True)
+    mealplan = models.CharField(max_length=50, default='', blank=True)
+
+    def __str__(self):
+        return self.name
--- dietician/views.py
+++ dietician/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 import numpy as np
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
+from .models import Report
 # Create your views here.
 
 
@@ -10,7 +11,10 @@ def dietician(request):
     prediction = None
     mealplan = None
     e = None
-    global g, d
+    g = None
+    d = None
+    bmi = None
+    remark = None
     df = pd.read_excel(
         r'D:\Study Material\SEM 8\CSE 445\Capstone Project\Capstone_Project\Live_Fit\media\Diet.xlsx')
 
@@ -25,6 +29,9 @@ def dietician(request):
     tree.fit(X_train.values, Y_train.values)
 
     if request.method == 'POST':
+        name = request.POST['name']
+        weight = request.POST['weight']
+        height = request.POST['height']
         age = request.POST['age']
         gender = request.POST['gender']
         exercise = request.POST['exercise']
@@ -35,7 +42,7 @@ def dietician(request):
         elif diabetic == "yes":
             d = 2
 
-        if gender == 'Male':
+        if gender == 'male':
             g = 1
         else:
             g = 2
@@ -53,5 +60,22 @@ def dietician(request):
         prediction = tree.predict([[g, age, e, d]])
 
         mealplan = str(prediction[0]).split('\n')
+        bmi = int(weight) / (int(height)/100)**2
 
-    return render(request, 'dietician/dietician.html', {'mealplan': mealplan})
+        if bmi <= 18.4:
+            remark = "You are underweight."
+        elif bmi <= 24.9:
+            remark = "You are healthy."
+        elif bmi <= 29.9:
+            rmark = "You are over weight."
+        elif bmi <= 34.9:
+            remark = "You are severely over weight."
+        elif bmi <= 39.9:
+            remark = "You are obese."
+        else:
+            remark = "You are severely obese."
+        r = Report(name=name, age=age, weight=weight, height=height,
+                   bmi=bmi, gender=gender, diabetic=diabetic, exercise=exercise, remark=remark, mealplan=mealplan)
+        r.save()
+
+    return render(request, 'dietician/dietician.html', {'mealplan': mealplan, 'bmi': bmi, 'remark': remark})
