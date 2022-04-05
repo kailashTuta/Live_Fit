6d14ac342477a0bb9f26d3ac2c82bb00cf8d5cab
kailashtuta <kailashtuta2000@gmail.com>
2022-04-05 08:08:37 +0530

    Added Pagination

--- workout_helper/views.py
+++ workout_helper/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Workout
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 
 def workouts(request):
-    pass
+    workouts = Workout.objects.all().order_by('-id')
+
+    paginator = Paginator(workouts, 12)
+    page_num = request.GET.get('page', 1)
+    workouts = paginator.get_page(page_num)
+    context = {'workouts': workouts}
+    return render(request, 'workout_helper/workouts.html', context)
